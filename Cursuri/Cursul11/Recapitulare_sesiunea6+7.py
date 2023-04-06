7a183729e2ff2ecea09e692ec4dbc7fe0e561374
Cosmin <cosminmoraru1996@gmail.com>
2023-04-06 18:59:00 +0300

    Add Curs 8

---
+++ Cursuri/Cursul11/Recapitulare_sesiunea6+7.py
@@ -0,0 +1,23 @@
+"""
+Programarea OOP este o modalitate prin care putem sa structuram prop. si comportamentul unei entitati din viata reala intr-o structura logica
+ ce functioneaza ca un template/ schita.
+
+ Acel template va serva drept model pentru create unor entitati care sa reprezinte un exemplu concret di nviata reala/.
+
+ Structura logica ce defineste entitatea se numeste clasa si reprezinta un tipa care descrie atributele pe care ar trebui sa le aiba entitatea si respectiv actiunile
+ pe care le paote face entitatea.
+
+Atunci cand cream o entitate propriu zisa pe baza acelei clase(scheme logice) spunem ca am creat un obiect modelat dupa aceea schema
+
+Crearea obictului pe baza unei clase se numeste instantiere, iar obiectul este o instanta a unei clase.
+
+Atucni cand se creaza obiectul de fapt se rezerva o adresa de memori care are access la toare atributele si functiile din clasa care sta la baza obiectului.
+
+In momentul in care noi vom pupula atributele obiectului, de fapt noi vom da valori efective pentru acele atribute care
+
+Esista posi
+
+
+
+
+"""
