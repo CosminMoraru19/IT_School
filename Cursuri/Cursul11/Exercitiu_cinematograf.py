9921f9ef0926577ed8df40465f3f8154acfb2cf8
Cosmin <cosminmoraru1996@gmail.com>
2023-04-11 19:05:14 +0300

    Add Curs 8

--- Cursuri/Cursul11/Exercitiu_cinematograf.py
+++ Cursuri/Cursul11/Exercitiu_cinematograf.py
@@ -1,3 +1,16 @@
+"""
+Tema pentru acasa:
+
+Creati o clasa care sa simuleze gestiunea unui cinematograf.
+
+Clasa trebuie sa aiba atribute, metode si constructor explicit (functionalitatea constructorului explicit o veti alege voi)
+
+Va trebui sa definiti metode simple, metode cu parametrii si metode cu parametri si return
+
+Din clasa respectiva veti instantia obiecte si va veti juca cu ele in functie de diverse scenarii pe care vreti sa le faceti (introducere film in gestiune, rulare film, cumparare bilete, rezervare bilete etc)
+
+Daca este posibil definit si functii cu numar nedefinit de parametrii (args / kwargs)
+"""
 class Cinema:
     def init(self, nume, adresa, capacitate):
         self.nume = nume
