45acb340c7187b11b46b25e43bd749df04913baf
Cosmin <cosminmoraru1996@gmail.com>
2023-04-25 18:56:53 +0300

    Add Curs 8

---
+++ Cursuri/Recap_Sesiunea9_plus_10/Exercitii_elefant.py
@@ -0,0 +1,17 @@
+"""
+Unit test este un nivel de testare care presupune testarea celor mai mici bucati functionale din cod
+
+Esita mai multe niveluri de testare dupa cum urmeaza:
+
+a) Unit testing = primul nivel de testare care evalueaza functionarea corecta a celor mai mici si functionale
+b) component testing= al doilea nivel de testare care evalueaza functionarea corecta a unor module finalizate din cod
+c)integration testing
+d) system testing
+e)accepptance testing
+
+Ex unitesting: verifica faptul ca se calculeaza corect aplicarea voucherului in cosul de cumparaturi
+
+Ex de component testing: verifica faptul ca toate functionalitatile din cosul de cuparaturi, functioneaza corect.
+
+
+"""
