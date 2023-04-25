74d49edb421e300b44d19a23f8344f9b1aab29b5
Cosmin <cosminmoraru1996@gmail.com>
2023-04-25 19:00:50 +0300

    Add Curs 8

--- Cursuri/Recap_Sesiunea9_plus_10/Exercitii_elefant.py
+++ Cursuri/Recap_Sesiunea9_plus_10/Exercitii_elefant.py
@@ -4,8 +4,13 @@ Unit test este un nivel de testare care presupune testarea celor mai mici bucati
 Esita mai multe niveluri de testare dupa cum urmeaza:
 
 a) Unit testing = primul nivel de testare care evalueaza functionarea corecta a celor mai mici si functionale
+
+in general testarea unitara este facuta de echipa de dezvoltare
 b) component testing= al doilea nivel de testare care evalueaza functionarea corecta a unor module finalizate din cod
-c)integration testing
+c)integration testing = al treilea nivel de testare care asigura comunicarea buna dintre componente sau sisteme
+                Exista 2 tipuri de testare de integrare:
+                - component integration testing- > evalueaza comunicarea corecta dintre 2 componente.
+                -system inegration testing - evalueaza comunicarea corecta intre doua sisteme.
 d) system testing
 e)accepptance testing
 
