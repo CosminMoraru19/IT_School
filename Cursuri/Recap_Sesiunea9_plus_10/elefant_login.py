453c4f54d54665aee624a7da4810126211951183
Cosmin <cosminmoraru1996@gmail.com>
2023-04-25 19:56:49 +0300

    Add Curs 8

--- Cursuri/Recap_Sesiunea9_plus_10/elefant_login.py
+++ Cursuri/Recap_Sesiunea9_plus_10/elefant_login.py
@@ -13,13 +13,14 @@ class Elefant_Login(unittest.TestCase):
 
     CONNECT_BUTTON = (By.CLASS_NAME, "my-account-link hidden-xs")
     COOCKIES_CLOSE = (By.ID, "CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll")
+    CONNECT_BUTTON_Modal = (By.LINK_TEXT, "Conectare")
     def setUp(self) -> None:
         self.chrome = webdriver.Chrome(executable_path=ChromeDriverManager().install())
         self.chrome.maximize_window()
         self.chrome.get("https://www.elefant.ro")
         try:
-            cookie_accept_button = WebDriverWait(self.chrome, 5).until(EC.presence_of_element_located(*self.COOCKIES_CLOSE))
-            cookie_accept_button.click()
+            accept_cookies_button = self.chrome.find_element(By.ID, "CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll")
+            accept_cookies_button.click()
         except:
             pass
     def tearDown(self) -> None:
@@ -27,6 +28,7 @@ class Elefant_Login(unittest.TestCase):
 
     def test_login_to_web_invalid_credentials(self):
         self.chrome.find_element(*self.CONNECT_BUTTON).click()
+        self.chrome.find_element(*self.CONNECT_BUTTON_Modal).click()
 
 
 
